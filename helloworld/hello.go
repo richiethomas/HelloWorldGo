c8f8cdbfd9d4aa4d019b908eb89a3d45ec0f8989
Richie Thomas <rickthomas1980@gmail.com>
2025-02-07 09:35:12 -0800

    Update Hello func

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,6 +2,16 @@ package helloworld
 
 import "fmt"
 
-func Hello() {
-	fmt.Println("Hello, World!")
+type Greeter struct {
+	Message string
+}
+
+func NewGreeter() *Greeter {
+	return &Greeter{
+		Message: "Hello, World 2!",
+	}
+}
+
+func (g *Greeter) Greet() {
+	fmt.Println(g.Message)
 }
